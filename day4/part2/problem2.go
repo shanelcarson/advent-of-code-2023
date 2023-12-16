1912c73a4381d3f90f3d0a326c1bf8c1b06d9ad4
Shane Carson <shanecarson98@gmail.com>
2023-12-15 21:12:41 -0800

    day4 part2 fix

--- day4/part2/problem2.go
+++ day4/part2/problem2.go
@@ -20,57 +20,54 @@ func main() {
 
 	sum := 0
 	totalCards := make(map[int]int)
-	playing := true
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
 		tokens := strings.Split(line, ": ")
 		cardId := strings.Split(tokens[0], " ")
 		id, _ := strconv.Atoi(cardId[len(cardId)-1])
-		// fmt.Println("Card Id", id)
 		if _, ok := totalCards[id]; ok {
 			totalCards[id] = totalCards[id] + 1
 		} else {
 			totalCards[id] = 1
 		}
 
-			numStrs := strings.Split(tokens[1], " | ")
-			cardNums := strings.Split(numStrs[0], " ")
-			myNums := strings.Split(numStrs[1], " ")
+		numStrs := strings.Split(tokens[1], " | ")
+		cardNums := strings.Split(numStrs[0], " ")
+		myNums := strings.Split(numStrs[1], " ")
 
-			numMap := make(map[string]bool)
-			for _, num := range cardNums {
-				num = strings.Trim(num, " ")
-				if num == "" {
-					continue
-				}
-				numMap[num] = false
+		numMap := make(map[string]bool)
+		for _, num := range cardNums {
+			num = strings.Trim(num, " ")
+			if num == "" {
+				continue
 			}
-			for _, num := range myNums {
-				num = strings.Trim(num, " ")
-				if num == "" {
-					continue
-				}
-				if value, ok := numMap[num]; ok && !value {
-					numMap[num] = true
-				}
+			numMap[num] = false
+		}
+		for _, num := range myNums {
+			num = strings.Trim(num, " ")
+			if num == "" {
+				continue
 			}
+			if value, ok := numMap[num]; ok && !value {
+				numMap[num] = true
+			}
+		}
 
-			winningMatches := 0
-			for _, match := range numMap {
-				if match {
-					winningMatches += 1
-				}
+		winningMatches := 0
+		for _, match := range numMap {
+			if match {
+				winningMatches += 1
 			}
-			if winningMatches > 0 {	
-				for i := 1; i <= winningMatches; i++ {
-					if _, ok := totalCards[id+i]; !ok {
-						totalCards[id+i] = 0
-					}
-					totalCards[id+i] += totalCards[id]
+		}
+		if winningMatches > 0 {
+			for i := 1; i <= winningMatches; i++ {
+				if _, ok := totalCards[id+i]; !ok {
+					totalCards[id+i] = 0
 				}
+				totalCards[id+i] += totalCards[id]
 			}
+		}
 	}
-	fmt.Println(totalCards)
 	for _, cardCount := range totalCards {
 		sum += cardCount
 	}
